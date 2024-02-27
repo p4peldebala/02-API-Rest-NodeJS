2cb4799107d284faa7e42e8861f6ed216ceed3b5
Diogo Eduardo <dev.juniordiogo@gmail.com>
2024-02-27 19:45:35 -0300

    Production config

--- src/database.ts
+++ src/database.ts
@@ -1,21 +1,21 @@
-
-import { knex as setupKnex, Knex} from "knex";
-import { env } from "./env";
+import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 // resgatando nossa variaveis ambiente => atraves do proc
 
 export const configKnex: Knex.Config = {
-    client: 'sqlite',
-    connection:{
-        // resgatando nossa variaveis ambiente => atraves do process.env
-        
-        filename: env.DATABASE_URL
-    }, 
+    client: env.DATABASE_CLIENT,
+    connection:
+        env.DATABASE_CLIENT === 'sqlite'
+            ? {
+                  filename: env.DATABASE_URL,
+              }
+            : env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
-        directory: './database/migrations'
-    }
+        directory: './database/migrations',
+    },
 }
 
 export const knex = setupKnex(configKnex)
--- src/env/index.ts
+++ src/env/index.ts
@@ -4,8 +4,10 @@ import * as zod from 'zod'
 
 const envSchema = zod.object({
     NODE_ENV: zod.enum(["development", "test", "production"]).default('production'),
+    DATABASE_CLIENT: zod.enum(['sqlite', 'pg']).default('sqlite'),
     DATABASE_URL: zod.string(),
-    PORT: zod.number().default(3333)
+    PORT: zod.coerce.number().default(3333)
+
 })
 
 const _env = envSchema.safeParse(process.env)
